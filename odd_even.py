d30245ee5cd9c9f48ade5f1540b4c1d6a25b274d
Manoj G <manojggm14@gmail.com>
2026-02-05 09:28:11 +0530

    Python_Code Branch

---
+++ odd_even.py
@@ -0,0 +1,11 @@
+num = int(input("Enter number:"))
+
+if (num %2 != 0):
+	print("Given Number is Odd ")
+
+elif (num %2 == 0):
+	print("Given Number is Even")
+
+else:
+	print("Enter Valid Number")
+
